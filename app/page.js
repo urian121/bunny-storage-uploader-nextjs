ed5df21bcbb8056794eb264d925078155578bd11
URIAN VIERA <urian1213viera@gmail.com>
2025-12-05 16:24:56 -0500

    actualizando titulo

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="w-full max-w-5xl rounded-xl bg-white p-8 shadow-sm dark:bg-zinc-900">
-        <h1 className="text-3xl font-semibold text-zinc-900 dark:text-white text-center">Subir imagen con Next.js</h1>
+        <h1 className="text-3xl font-semibold text-zinc-900 dark:text-white text-center">Subir imagen con Next.js a Bunny Storage</h1>
         <hr className="my-4 border-zinc-200 dark:border-zinc-800" />
         <div className="grid grid-cols-1 md:grid-cols-2 md:divide-x md:divide-zinc-200 dark:md:divide-zinc-800">
           <section className="md:pr-6">
